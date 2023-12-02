7264284957c09b155355cb75d4c5a51926b29086
nsenave <nicolas.senave@gmail.com>
2023-12-02 06:52:53 +0100

    adjust daily script

--- 2023/daily.py
+++ 2023/daily.py
@@ -1,3 +1,4 @@
+import os.path
 import unittest
 
 def line_split(line: str) :
@@ -5,11 +6,13 @@ def line_split(line: str) :
     return res
 
 def parse_input(file_path: str) :
+    if not os.path.exists(file_path) :
+        return None
     with open(file_path, 'r') as f :
         return list(map(line_split, f.read().split('\n')))
 
 puzzle = parse_input('input.txt')
-example = parse_input('example/input.txt')
+example = parse_input('input-example.txt')
 
 print("Example input:")
 print(example)
