99f51fc71b6f2fdc12ba4db29a26e69764c7e0ae
nsenave <nicolas.senave@gmail.com>
2025-11-29 00:10:26 +0100

    2020 day 1 go

---
+++ 2020/day_01/day-01.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "os"
+    "bufio"
+    "strconv"
+)
+
+func main() {
+    exampleNumbers := parseInput("input-example.txt")
+    fmt.Printf("Example input: %v\n", exampleNumbers)
+    personalNumbers := parseInput("input.txt")
+
+    fmt.Println("")
+
+    fmt.Println("--- Part One ---")
+    fmt.Println("Example")
+    part1(exampleNumbers)
+    fmt.Println("Input")
+    part1(personalNumbers)
+
+    fmt.Println("")
+
+    fmt.Println("--- Part Two ---")
+    fmt.Println("Example")
+    part2(exampleNumbers)
+    fmt.Println("Input")
+    part2(personalNumbers)
+}
+
+// Input parsing
+
+func parseInput(fileName string) []int {
+    inputFile, err := os.Open(fileName)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer inputFile.Close()
+
+    scanner := bufio.NewScanner(inputFile)
+    var numbers []int
+
+    for scanner.Scan() {
+        line := scanner.Text()
+        n, err := strconv.Atoi(line)
+        if err != nil {
+            log.Printf("Error trying to convert: '%s'\n", line)
+            log.Fatal(err)
+        }
+        numbers = append(numbers, n)
+    }
+
+    if err := scanner.Err(); err != nil {
+        log.Fatal(err)
+    }
+
+    return numbers
+}
+
+// Utility functions
+
+func product(numbers []int) int {
+    result := 1
+    for _, n := range numbers {
+        result *= n
+    }
+    return result
+}
+
+// Part One
+
+func part1(numbers []int) {
+    matchingNumbers := findNumbersThatAddTo2020(numbers)
+    fmt.Printf("Matching numbers: %v\n", matchingNumbers)
+    fmt.Printf("Answer: %d\n", product(matchingNumbers))
+}
+
+func findNumbersThatAddTo2020(numbers []int) []int {
+    numbersCount := len(numbers)
+    for i := range numbersCount {
+        n1 := numbers[i]
+        for j := i; j < numbersCount; j++ {
+            n2 := numbers[j]
+            if (n1 + n2 == 2020) {
+                return []int{n1, n2}
+            }
+        }
+    }
+    log.Fatal("Didn't fund two numbers thats add to 2020.")
+    return []int{}
+}
+
+// Part Two
+
+func part2(numbers []int) {
+    matchingNumbers := findThreeNumbersThatAddTo2020(numbers)
+    fmt.Printf("Matching numbers: %v\n", matchingNumbers)
+    fmt.Printf("Answer: %d\n", product(matchingNumbers))
+}
+
+func findThreeNumbersThatAddTo2020(numbers []int) []int {
+    numbersCount := len(numbers)
+    for i := range numbersCount {
+        n1 := numbers[i]
+        for j := i; j < numbersCount; j++ {
+            n2 := numbers[j]
+            for k := j; k < numbersCount; k++ {
+                n3 := numbers[k]
+                if (n1 + n2 + n3 == 2020) {
+                    return []int{n1, n2, n3}
+                }
+            }
+        }
+    }
+    log.Fatal("Didn't fund two numbers thats add to 2020.")
+    return []int{}
+}
