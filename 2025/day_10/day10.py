2f10396c6ed963b7b49550f55b884dbd2433e842
nsenave <nicolas.senave@gmail.com>
2025-12-11 00:12:45 +0100

    2025 10 part 1

--- 2025/day_10/day10.py
+++ 2025/day_10/day10.py
@@ -1,6 +1,8 @@
 import os.path
 import unittest
 import time
+import itertools
+
 
 def parse_button(str_button):
     return tuple(map(int, str_button[1:-1].split(',')))
@@ -20,6 +22,49 @@ def parse_input(file_path: str) :
         return list(map(line_split, f.read().split('\n')))
 
 
+
+# Press 0 button, then 1 button, then 2 buttons etc.
+def ordered_inputs(buttons_number):
+    """Yields lists of inputs (press button at index i or not)"""
+    yield [False] * buttons_number
+    for press_number in range(1, buttons_number + 1):
+        for positions in itertools.combinations(range(buttons_number), press_number):
+            inputs = [False] * buttons_number
+            for p in positions:
+                inputs[p] = True
+            yield inputs
+
+def init_lights(buttons_number):
+    """Returns list of initial light values (all False i.e. off)."""
+    return [False] * buttons_number
+
+def bool_value(indicator_light: str):
+    return indicator_light == '#'
+
+def press_button(button: tuple, lights: list):
+    for i in button:
+        lights[i] = not lights[i]
+
+def apply(inputs, buttons, lights):
+    for press, button in zip(inputs, buttons):
+        if press:
+            press_button(button, lights)
+
+def matches_indicator(lights: list, indicator_lights: str) -> bool:
+    for indicator_light, light in zip(indicator_lights, lights):
+        if bool_value(indicator_light) != light:
+            return False
+    return True
+
+def find_min_press_number(x):
+    indicator_lights, buttons, _ = x
+    lights_length = len(indicator_lights)
+    buttons_number = len(buttons)
+    for inputs in ordered_inputs(buttons_number):
+        lights = init_lights(lights_length)
+        apply(inputs, buttons, lights)
+        if matches_indicator(lights, indicator_lights):
+            return sum(inputs)
 
 def r1(puzzle_input, debug=False) :
     if puzzle_input is None:
@@ -61,13 +106,13 @@ if __name__ == '__main__':
     print("--- Part One ---")
     t0 = time.time()
     print(f"Example result: {r1(example, True)}")
-    #print(f"Puzzle answer:  {r1(puzzle)}")
+    print(f"Puzzle answer:  {r1(puzzle)}")
     t1 = time.time()
     print(f"Part one computed in {t1 - t0} seconds.")
 
     print("--- Part Two ---")
     t0 = time.time()
     print(f"Example result: {r2(example, True)}")
-    #print(f"Puzzle answer:  {r2(puzzle)}")
+    print(f"Puzzle answer:  {r2(puzzle)}")
     t1 = time.time()
     print(f"Part two computed in {t1 - t0} seconds.")
