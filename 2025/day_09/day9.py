3c8a11f618506e17897d2da474c1a7330c96bdd6
nsenave <nicolas.senave@gmail.com>
2025-12-10 01:25:33 +0100

    day 9 part 1

--- 2025/day_09/day9.py
+++ 2025/day_09/day9.py
@@ -1,6 +1,7 @@
 import os.path
 import unittest
 import time
+import re
 
 def line_split(line: str) :
     return tuple(map(int, line.split(',')))
@@ -17,12 +18,10 @@ def parse_input(file_path: str) :
 def area(coord1, coord2):
     x1, y1 = coord1
     x2, y2 = coord2
-    return abs(x2 - x1) * abs(y2 - y1)
+    return (abs(x2 - x1) + 1) * (abs(y2 - y1) + 1)
 
 def area2(coords):
-    x1, y1 = coords[0]
-    x2, y2 = coords[1]
-    return abs(x2 - x1) * abs(y2 - y1)
+    return area(coords[0], coords[1])
 
 def list_pairs(points):
     pairs = []
@@ -35,25 +34,106 @@ def list_pairs(points):
 def r1(puzzle_input, debug=False) :
     if puzzle_input is None:
         return None
-    res = 0
-    pairs = list_pairs(puzzle_input)
-    pairs.sort(key=lambda pair: area(pair[0], pair[1]))
-    areas = [area2(coords) for coords in pairs]
-    print(pairs)
-    print(areas)
-    for x in puzzle_input :
-        pass
+    return max([area2(coords) for coords in list_pairs(puzzle_input)])
+
+
+
+def arrange(puzzle_input, n, m) -> list:
+    res = []
+    for i in range(n):
+        line = []
+        for j in range(m):
+            if (j, i) in puzzle_input:
+                line.append("#")
+            else:
+                line.append(".")
+        res.append(line)
     return res
 
+def grid_to_string(grid: list) -> str:
+    return '\n'.join(list(map(lambda line: ''.join(line), grid)))
 
+def switch_tile(tile: str):
+    if (tile == '.'): return 'X'
+    if (tile == 'X'): return '.'
+    raise ValueError(tile)
+
+def horizontal_lines(grid, n, m):
+    for i in range(n):
+        line = grid[i]
+        assert line.count('#') % 2 == 0
+        tile = '.'
+        for j in range(m):
+            if line[j] == '#':
+                tile = switch_tile(tile)
+                continue
+            if tile == 'X':
+                line[j] = tile
+
+def vertical_lines(grid, n, m):
+    for j in range(m):
+        red_count = 0
+        tile = '.'
+        for i in range(n):
+            if grid[i][j] == '#':
+                red_count += 1
+                tile = switch_tile(tile)
+                continue
+            if tile == 'X':
+                grid[i][j] = tile
+        assert red_count % 2 == 0
+
+def count_groups(line):
+    return len(re.findall(r'\.+', ''.join(line)))
+
+def fill(grid, n, m):
+    for i in range(n):
+        line = grid[i]
+        dot_groups = count_groups(line)
+        if dot_groups == 1 or (dot_groups % 2 == 0):
+            continue
+        tile = '.'
+        j = 0
+        while j < m:
+            if line[j] != '.':
+                while line[j] != '.':
+                    j += 1
+                tile = switch_tile(tile)
+            if tile == 'X':
+                line[j] = tile
+            j += 1
+
+def is_in_tiles(pair, grid):
+    y1, x1 = pair[0]
+    y2, x2 = pair[1]
+    if x1 > x2: x1, x2 = x2, x1
+    if y1 > y2: y1, y2 = y2, y1
+    for i in range(x1, x2):
+        for j in range(y1, y2):
+            if grid[i][j] == '.':
+                return False
+    return True
 
 def r2(puzzle_input, debug=False) :
     if puzzle_input is None:
         return None
-    res = 0
-    for x in puzzle_input :
-        pass
-    return res
+
+    n = max([coords[1] for coords in puzzle_input]) + 2
+    m = max([coords[0] for coords in puzzle_input]) + 3
+    # Note: + 2 / + 3 to match given examples representations
+    grid = arrange(puzzle_input, n, m)
+    horizontal_lines(grid, n, m)
+    vertical_lines(grid, n, m)
+    fill(grid, n, m)
+    if debug:
+        print(grid_to_string(grid))
+    # works for example but actual input is too large to be stored in a 2d array
+    # todo: coords compression
+    # https://phuongdinh1411.github.io/cses-analyses/problem_soulutions/graph_algorithms/coordinate_compression_analysis
+
+    pairs = list_pairs(puzzle_input)
+    filtered_pairs = list(filter(lambda pair: is_in_tiles(pair, grid), pairs))
+    return max([area2(coords) for coords in filtered_pairs])
 
 
 
@@ -77,7 +157,7 @@ if __name__ == '__main__':
     print("--- Part One ---")
     t0 = time.time()
     print(f"Example result: {r1(example, True)}")
-    #print(f"Puzzle answer:  {r1(puzzle)}")
+    print(f"Puzzle answer:  {r1(puzzle)}")
     t1 = time.time()
     print(f"Part one computed in {t1 - t0} seconds.")
 
